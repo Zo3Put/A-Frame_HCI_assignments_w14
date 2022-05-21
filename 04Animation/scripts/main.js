8ca4dfdaba751c04e5479b4812f2db4fc9ef2724
Zo3Put <105966842+Zo3Put@users.noreply.github.com>
2022-05-21 21:48:34 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,10 +2,11 @@
 
 // // changing individual properties with code and using setInterval
 var rotationSpeed = 0.001;
+var scaleUp = 5;
 var myOtherBox = document.getElementById('myOtherBox');
 
+
 function spin(){
-  myOtherBox.object3D.rotation.x -= rotationSpeed;
   myOtherBox.object3D.rotation.y -= rotationSpeed;
   myOtherBox.object3D.rotation.z -= rotationSpeed;
   console.log(myOtherBox.object3D.rotation.x);
@@ -13,4 +14,9 @@ function spin(){
   console.log(myOtherBox.object3D.rotation.z);
 }
 
+function scale(){
+  myOtherBox.object3D.scale.x + scaleUp;
+}
+
 setInterval(spin, 16); //equivalent to 60 fps
+setInterval(scale, 16);
