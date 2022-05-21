0550f54b7ec1d4d38636d5f3607d318912158121
Zo3Put <105966842+Zo3Put@users.noreply.github.com>
2022-05-21 22:18:06 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,25 +2,25 @@
 
 // // changing individual properties with code and using setInterval
 var rotationSpeed = 0.10;
-var rotationSpeedX = 0.05;
+var rotationSpeedX = 0.05; //langzamere snelheid op x-as
 var myOtherBox = document.getElementById('myOtherBox');
 
 
 function spin(){
-  myOtherBox.object3D.rotation.x += rotationSpeedX;
-  myOtherBox.object3D.rotation.y -= rotationSpeed;
-  myOtherBox.object3D.rotation.z -= rotationSpeed;
+  myOtherBox.object3D.rotation.x += rotationSpeedX; //rotatie met de klok mee op de x-as
+  myOtherBox.object3D.rotation.y -= rotationSpeed; //rotatie tegen de klok in op de y-as
+  myOtherBox.object3D.rotation.z -= rotationSpeed; //rotatie tegen de klok in op de z-as
   console.log(myOtherBox.object3D.rotation.x);
   console.log(myOtherBox.object3D.rotation.y);
   console.log(myOtherBox.object3D.rotation.z);
 }
 
-if(myOtherBox.object3D.rotation.x <= -100){
-  myOtherBox.object3D.rotation.x -= rotationSpeedX;
+if(myOtherBox.object3D.rotation.x <= -100){ //als de rotatie op de x-as kleiner is of gelijk is aan 100
+  myOtherBox.object3D.rotation.x -= rotationSpeedX; //de rotatie gaat omkeren en langzamer op de x-as 
 }
 
-if(myOtherBox.object3D.rotation.y <= 100){
-  myOtherBox.object3D.rotation.y -= 0.01;
+if(myOtherBox.object3D.rotation.y <= 100){ //als de rotatie op de y-as kleiner is of gelijk is aan 100
+  myOtherBox.object3D.rotation.y += 0.05; //de rotatie gaat omkeren en langzamer op de y-as
 }
   
 setInterval(spin, 16); //equivalent to 60 fps
